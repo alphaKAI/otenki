e2ee3edbc44d0430f0cd8b7992b7d77158be6f52
alphaKAI <alpha.kai.net@alpha-kai-net.info>
2018-12-25 23:44:39 +0900

    update doc for -url option

--- src/main.ml
+++ src/main.ml
@@ -14,7 +14,7 @@ let command =
   Command.basic
     ~summary:"Command line weather forcast viewer"
     [%map_open
-      let url = flag "url" (optional_with_default tsukuba_url string) ~doc: "表示したい地域に対応するtenki.jpの3時間天気予報のURL"
+      let url = flag "url" (optional_with_default tsukuba_url string) ~doc: "表示したい地域に対応するtenki.jpの3時間天気予報のURL(デフォルトはつくば市)"
       and days = flag "days" (optional_with_default 2 int) ~doc: "表示したい日数1~3(デフォルトは2)"
       and conky = flag "conky" no_arg ~doc:"conkyで使いたい場合指定する"
       and update = flag "update" no_arg ~doc:"強制的にキャッシュを更新したい場合指定する" in
